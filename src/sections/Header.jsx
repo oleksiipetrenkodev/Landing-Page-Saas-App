fe7a87e2f7a231d1c0e9ac5b3c23d1a43bb93e0e
oleksii-petrenko-metagenics <oleksiipetrenko@metagenics.com>
2025-09-29 15:23:07 +0300

    added handle scroll

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
 
 const NavLink = ({ title }) => (
@@ -10,8 +10,27 @@ const NavLink = ({ title }) => (
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [hasScrolled]);
+
   return (
-    <header className="fixed top-0 left-0 z-50 flex w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 flex w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={115} height={55} alt="Logo"></img>
