ab4eb18016c81142ef1533d079e7da2bc010b954
oleksii-petrenko-metagenics <oleksiipetrenko@metagenics.com>
2025-09-29 22:33:14 +0300

    added Faq sections, installed SlideDown

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      {/* <Header />
+      <Header />
       <Hero />
-      <Features /> */}
+      <Features />
       <Pricing />
+      <Faq />
     </main>
   );
 };
